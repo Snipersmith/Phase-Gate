9b74b62d264bd54bfa3938875117f245c139aaea
Snipersmith <okaforstanley147@gmail.com>
2025-10-27 14:58:02 +0100

    phase gate

---
+++ python/DAY 1/Arithmetic app/substraction_app.py
@@ -0,0 +1,17 @@
+import random
+
+print ("Welcome to my substraction app")
+points = 0
+
+for index in range(1, 10):
+	first_number = random.randint(0, 100)
+	second_number = random.randint(0, 100)
+
+print (f"Question 1 What is {10} - {5}")
+
+for attempt in range (1, 3):
+	answer = int(input("Your answer: "))
+
+	if answer == first_number - second_number:
+		print ("correct")
+		points = +1
